56a8e67ae75006e55d9311c414249a1341d343e3
mieprenzo <72381@hoornbeeck.nl>
2025-06-27 10:57:28 +0200

    pages migration added

---
+++ database/migrations/2025_06_27_085556_create_pages_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->longText('content');
+            $table->foreignId('notebook_id')->constrained('notebooks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pages');
+    }
+};
